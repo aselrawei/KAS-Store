ca5b78eb21cc20c866f9c26713ef4d18a8e6a89a
Ashraf Gamal Rawei <57330737+aselrawei@users.noreply.github.com>
2024-10-26 00:15:30 +0300

    Update elrawy.py

--- elrawy.py
+++ elrawy.py
@@ -14,28 +14,13 @@ api_hash = '127b00ff9bc362284fcafc2587ac066a'
 
 # Initialize Telethon client
 client = TelegramClient('test1', api_id, api_hash)
-
 static_keywords = [
-    'banknxteg', ' aib ', ' efg ', '.com.sa',
-    'egypt', 'cairo', 'misr', ' nbe ', ' cib ', ' qnb ', ' alahly ',
-    'fawry', 'efinance', 'saudi', ' ksa ', ' sdif ', ' SAMA ',
-    'arab', 'arab bank', 'national commercial bank', 'rajhi',
-    'faisal', 'sabb', 'riyad bank', ' bsf ', 'alinma',
-    'albilad', 'bank albilad', 'albaraka', 'mashreq',
-    'emirates', ' uae ', 'hsbc', 'citibank',
-    'hajj', 'mecca', 'riyadh', 'dammam', 'jeddah',
-    'vision 2030', 'arab national bank',
-    'egfincirt', ' cbe ', 'myegbank', ' bdc ', 'du caire',
-    'scbank', '.eg', '.com.eg', '.gov.sa', '.gov.eg',
-    'alahlybank', 'cibeg.com', 'ahliunited.com', 'bankaudi.com.eg',
-    'bank-abc', 'banquemisr', 'bmonline',
-    'faisalbank', 'hsbc', 'qnbalahli', 'scbank'
-]
+    'fortinet', 'forcepoint','apache' ,'paloalto', 'oracle','vmware','microsoft','redhat','citrix','ivanti','openshift','beyond trust','kaspersky','nextthink','xerox','chrome','firefox','adobe','cisco',' F5 ','veeam','zoom','webex'
+    ]
 
-channel_ids = ['breachdetector', 'ransomlook', 'RansomFeedNews', 'TheDarkWebInformer',
-               'cyberthint', 'yildizthreatnews', 'RansomwareNewsVX', 'ransomwarelive', 'Venari_By_BetterCyber']
+channel_ids = ['cveNotify','cvedetector']
 
-forward_channel_id = 'ElrawyDM1'  # Replace with the actual ID of the channel to forward to
+forward_channel_id = 'sncve2024'  # Replace with the actual ID of the channel to forward to
 
 async def is_message_identical(message_text):
     """Check if the message text already exists in the forward channel."""
@@ -50,13 +35,12 @@ async def search_and_alert(channel_id):
     try:
         messages = await client.get_messages(channel_id, limit=500)
         for message in messages:
-            if message.text:
+            if 'CVE-2024' in message.text:
                 for keyword in static_keywords:
                     if keyword.lower() in message.text.lower():
                         if not await is_message_identical(message.text):
-                            await client.send_message(forward_channel_id,
-                                                       f"===> Keyword [{keyword}] Found : === \n {message.text}")
-                            await asyncio.sleep(5)  # Delay after sending a message
+                          await client.send_message(forward_channel_id,f"===> New [ {keyword} ] CVE Found : === \n {message.text}")
+                          await asyncio.sleep(5)  # Delay after sending a message
                         break
     except FloodWaitError as e:
         print(f"Rate limit hit. Waiting for {e.seconds} seconds.")
@@ -79,6 +63,7 @@ def job():
 if __name__ == "__main__":
     # Schedule the job to run every 5 hours
     schedule.every(1).seconds.do(job)
+
     # Keep the script running
     while True:
         schedule.run_pending()
